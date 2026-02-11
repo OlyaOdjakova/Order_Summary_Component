ffb242d23e7b8b37d25fab61814ffc8fa47405bb
OlyaOdjakova <Olgica.Odzakova@students.finki.ukim.mk>
2026-02-11 20:04:16 +0100

    Format files with prettier

--- src/Buttons/cancelOrder.js
+++ src/Buttons/cancelOrder.js
@@ -2,12 +2,9 @@ import React from "react";
 import styles from "./buttons.module.css";
 
 function CancelOrder() {
-
-    return (
-        <button className={styles.cancelOrderButtonStyle}>
-            Cancel Order
-        </button>
-    )
+  return (
+    <button className={styles.cancelOrderButtonStyle}>Cancel Order</button>
+  );
 }
 
 export default CancelOrder;
--- src/Buttons/proceedToPayment.js
+++ src/Buttons/proceedToPayment.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import styles from "./buttons.module.css";
 
 function ProceedToPayment() {
-
-    return (
-        <button className={styles.proceedToPaymentButtonStyle}>
-            Proceed to Payment
-        </button>
-    )
+  return (
+    <button className={styles.proceedToPaymentButtonStyle}>
+      Proceed to Payment
+    </button>
+  );
 }
 
 export default ProceedToPayment;
