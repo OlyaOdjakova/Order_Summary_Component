1406b5770ec795f2007d25df6dbfd0fb5b2420e4
OlyaOdjakova <Olgica.Odzakova@students.finki.ukim.mk>
2026-02-12 00:14:29 +0100

    Add SpeedInsights

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import app from "./app";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
