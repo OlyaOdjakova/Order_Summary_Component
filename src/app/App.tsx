0ad6ae7259cd7f9afbd925b47bb2578d66c489a9
OlyaOdjakova <Olgica.Odzakova@students.finki.ukim.mk>
2026-02-12 00:07:27 +0100

    Add background images and align them

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./app.module.css";
 import OrderCard from "../order_card/OrderCard";
+import backgroundImage from "../images/pattern-background-desktop.svg";
 
 const App = () => {
   return (
-    <div className={styles.appJsStyle}>
+    <div className={styles.cardContainer}>
+      <img
+        src={backgroundImage}
+        className={styles.annualPlanCardImage}
+        alt="music"
+      />
+
+      <img
+        src={backgroundImage}
+        className={`${styles.annualPlanCardImage} ${styles.flippedImage}`}
+        alt="music flipped"
+      />
       <OrderCard />
     </div>
   );
