4bb8dbe60d26297c0a2b41499de7e937e458eabe
OlyaOdjakova <Olgica.Odzakova@students.finki.ukim.mk>
2026-02-13 14:51:05 +0100

    Remove not needed files and modify folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./app.module.css";
 import OrderCard from "./order_card/OrderCard";
-import backgroundImage from "./images/pattern-background-desktop.svg";
+import backgroundImage from "../../images/pattern-background-desktop.svg";
 
 const App = () => {
   return (
@@ -11,7 +11,6 @@ const App = () => {
         className={styles.annualPlanCardImage}
         alt="music"
       />
-
       <img
         src={backgroundImage}
         className={`${styles.annualPlanCardImage} ${styles.flippedImage}`}
